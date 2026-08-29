b6a2c848ae985796f8aa60f5642bc17a495ff540
Yurii <yurijlevchuk@gmail.com>
2026-08-29 19:15:18 +0200

    feat: add email confirmation flow

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,14 +1,17 @@
 import pytest
+from allauth.account.models import EmailAddress
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+
 
 User = get_user_model()
 
 
 @pytest.fixture
 def user(db):
-    return User.objects.create_user(email="user@example.com", password="SecurePass123!")
-
+    user = User.objects.create_user(email="user@example.com", password="SecurePass123!")
+    EmailAddress.objects.create(user=user, email=user.email, verified=True, primary=True)
+    return user
 
 @pytest.mark.django_db
 def test_login_page_loads(client):
