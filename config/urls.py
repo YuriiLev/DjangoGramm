06236d25394179dd376d410cefc05ac8ff9c957d
Yurii <yurijlevchuk@gmail.com>
2026-09-06 14:32:44 +0200

    fix: import debug toolbar urls only when debug is enabled

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -10,4 +10,9 @@ urlpatterns = [
     path("profiles/", include("profiles.urls")),
     path("posts/", include("posts.urls")),
     path("", feed, name="home"),
-] + debug_toolbar_urls()
+]
+
+if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+
+    urlpatterns += debug_toolbar_urls()
